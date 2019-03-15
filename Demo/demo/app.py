a8ae661a2df5e5aaed9671e372cfec1393ec4d46
StognienkoVladislav <stognienkovv@gmail.com>
2019-03-15 20:04:20 +0200

    aio demo config + db

--- Demo/demo/app.py
+++ Demo/demo/app.py
@@ -1,12 +1,27 @@
 import jinja2
+import asyncpgsa
 import aiohttp_jinja2
 
 from aiohttp import web
 from .routes import setup_routes
 
 
-async def create_app():
+async def create_app(config: dict):
     app = web.Application()
+    app['config'] = config
+
     aiohttp_jinja2.setup(app, loader=jinja2.PackageLoader('demo', 'templates'))
     setup_routes(app)
+
+    app.on_startup.append(on_start)
+    app.on_cleanup.append(on_shutdown)
     return app
+
+
+async def on_start(app):
+    config = app['config']
+    app['db'] = await asyncpgsa.create_pool(dsn=config['database_uri'])
+
+
+async def on_shutdown(app):
+    await app['db'].close()
--- Demo/entry.py
+++ Demo/entry.py
@@ -1,9 +1,37 @@
 
+import uvloop
+import asyncio
 import aiohttp
+import argparse
+import aioreloader
+
 from demo import create_app
+from demo.settings import load_config
 
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
-app = create_app()
+parser = argparse.ArgumentParser(description='Demo project')
+parser.add_argument('--host', help='Host to listen', default='0.0.0.0')
+parser.add_argument('--port', help='Port to accept connections', default=5000)
+parser.add_argument('--reload', action='store_true', help='Autoreload code on change')
+parser.add_argument('-c', '--config', type=argparse.FileType('r'), help='Path to config file')
+
+args = parser.parse_args()
+
+app = create_app(config=load_config(args.config))
+
+# for local.yaml config
+# python entry.py --reload -c local.yaml
+
+"""
+# If '--reload' in terminal as arg
+if args.reload:
+    print("Start with code reload")
+    aioreloader.start()
+    
+"""
+
 
 if __name__ == '__main__':
-    aiohttp.web.run_app(app, )
+    aioreloader.start()
+    aiohttp.web.run_app(app, host=args.host, port=args.port)
